0e2936f37fa307f2fd285aaff0b7312439e2abfb
Gonzalo G Campos <15249923+gonzalogcampos@users.noreply.github.com>
2021-05-12 17:44:18 +0200

    Restored

--- OptiMotionblur_B.py
+++ OptiMotionblur_B.py
@@ -6,57 +6,57 @@ import nuke
 # It memorizes the current value of motionblur for each node and sets it to 0
 # Then when you reactivate the motionblur recovers the last value.
 
-transform = True
-motionblur = True
-cornerpin = True
-roto = True
-
-
-
 #DEACTIVATION
-def OptiMotionblur_D():
-    for node in nuke.allNodes():
-        if (node.Class() == 'Transform' and transform) or (node.Class() == 'CornerPin2D' and cornerpin):
-            if not node.knob('aux_motionblur'):
-                auxKnob = nuke.Double_Knob('aux_motionblur', 'Aux Motionblur')
-                auxKnob.setEnabled(False)
-                auxKnob.setValue(node.knob('motionblur').getValue())
-                node.addKnob(auxKnob)
-                node.knob('motionblur').setValue(0)
-                node.knob('motionblur').setEnabled(False)
-        elif roto and node.Class() == 'Roto' and node.knob('motionblur_mode').getValue()==1:
-            if not node.knob('aux_motionblur'):
-                auxKnob = nuke.Double_Knob('aux_motionblur', 'Aux Motionblur')
-                auxKnob.setEnabled(False)
-                auxKnob.setValue(node.knob('global_motionblur').getValue())
-                node.addKnob(auxKnob)
-                node.knob('global_motionblur').setValue(0)
-                node.knob('global_motionblur').setEnabled(False)
-        elif motionblur and node.Class() == 'MotionBlur':
-            if not node.knob('aux_motionblur'):
-                auxKnob = nuke.Boolean_Knob('aux_motionblur', 'Aux Motionblur')
-                auxKnob.setEnabled(False)
-                auxKnob.setValue(node.knob('disable').getValue())
-                node.addKnob(auxKnob)
-                node.knob('disable').setValue(True)
-                node.knob('disable').setEnabled(False)
-
-
-#REACTIVATION
-def OptiMotionblur_A():
+def optiMotionblur():
     for node in nuke.allNodes():
         if node.Class() == 'Transform' or node.Class() == 'CornerPin2D':
-            if node.knob('aux_motionblur'):
+            was = True
+            if not node.knob('aux_motionblur'):
+                auxKnob = nuke.Double_Knob('aux_motionblur', 'Aux Motionblur')
+                auxKnob.setEnabled(False)
+                node.addKnob(auxKnob)
+                node.knob('aux_motionblur').setValue(node.knob('motionblur').getValue())
+                was = False
+
+            if not nuke.root().knob('root_motionblur').getValue():
+                node.knob('aux_motionblur').setValue(node.knob('motionblur').getValue())
+                node.knob('motionblur').setValue(0)
+                node.knob('motionblur').setEnabled(False)
+            else:
                 node.knob('motionblur').setValue(node.knob('aux_motionblur').getValue())
                 node.knob('motionblur').setEnabled(True)
-                node.removeKnob(node.knob('aux_motionblur'))
-        elif node.Class() == 'Roto' and node.knob('motionblur_mode').getValue()==1:
-            if node.knob('aux_motionblur'):
-                node.knob('global_motionblur').setValue(node.knob('aux_motionblur').getValue())
-                node.knob('global_motionblur').setEnabled(True)
-                node.removeKnob(node.knob('aux_motionblur'))
-        elif node.Class() == 'MotionBlur':
-            if node.knob('aux_motionblur'):
-                node.knob('disable').setValue(node.knob('aux_motionblur').getValue())
-                node.knob('disable').setEnabled(True)
-                node.removeKnob(node.knob('aux_motionblur'))
+
+def knobCallback():
+    try:
+        if nuke.thisKnob() == nuke.root().knob('root_motionblur'):
+            optiMotionblur()
+    except:
+        pass
+
+def defineRoot():
+    bk = nuke.Boolean_Knob('root_motionblur', 'Motionblur')
+    nuke.root().addKnob(bk)
+    nuke.root().knob('root_motionblur').setDefaultValue([1])
+    nuke.addKnobChanged(knobCallback, node=nuke.root())
+
+
+def onNodeCreate():
+    node = nuke.thisNode()
+
+    if not node.knob('aux_motionblur'):
+            auxKnob = nuke.Double_Knob('aux_motionblur', 'Aux Motionblur')
+            auxKnob.setEnabled(False)
+            auxKnob.setValue(node.knob('motionblur').getValue())
+            node.addKnob(auxKnob)
+
+    if not nuke.root().knob('root_motionblur').getValue():
+        node.knob('motionblur').setValue(0)
+        node.knob('motionblur').setEnabled(False)
+
+def conmuteOptiMotionblur():
+    nuke.root().knob('root_motionblur').setValue(not nuke.root().knob('root_motionblur').getValue())
+
+
+defineRoot()
+nuke.addOnUserCreate(onNodeCreate, nodeClass="Transform")
+nuke.addOnUserCreate(onNodeCreate, nodeClass="CornerPin2D")
